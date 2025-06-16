4704dbb4de0e4ae98407403912c909679863d611
Tobias Schäfer <tobias.schaefer@futalis.de>
2025-06-16 09:54:52 +0200

    Updates curl to 8.14.1
    Updates curl to the latest stable version, 8.14.1, across multiple PHP versions and Alpine Linux distributions.
    
    This update includes necessary changes to the APKBUILD files.
    
    Also, a patch has been applied to address an incompatibility issue with Inline TLS Assembly on Alpine 3.22 with zend_jit_ir.c
    
    Removes unneeded alpine3.22 curl builds from versions.json

--- versions.sh
+++ versions.sh
@@ -91,7 +91,10 @@ for version in "${versions[@]}"; do
 		if [ "$rcVersion" = '8.0' ] && [ "$suite" = 'bookworm' ]; then
 			continue
 		fi
-        if [ "$rcVersion" = '8.0' ] && [ "$suite" != 'alpine3.20' ]; then
+        if [ "$rcVersion" = '8.0' ] && [ "$suite" = 'alpine3.22' ]; then
+            continue
+        fi
+        if [ "$rcVersion" = '8.0' ] && [ "$suite" = 'alpine3.21' ]; then
             continue
         fi
 		for variant in cli apache fpm zts fpm-zts; do
